5376ef545458fdc2afd4730486a34724467614b1
norder <jeff@norjekudo.com>
2017-06-10 21:14:01 -0400

    second post

---
+++ src/main/java/Library.java
@@ -0,0 +1,10 @@
+package com.norjekudo;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
